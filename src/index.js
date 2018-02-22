d366f546b088202c0ff2249044b14ec16a4947b7
Your Name <you@example.com>
2018-02-22 22:46:06 +0300

    additional_5 decided

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    let config = [].concat.apply([], bracketsConfig),
+        stack = [],
+        bracket,
+        lastBracket,
+        character;
+
+    for(let i = 0; character = str[i]; i++){
+
+        bracket = config.indexOf(character);
+        lastBracket = config.lastIndexOf(character);
+
+        if(bracket === -1) return false;
+
+        if(bracket % 2 == 0 && stack[stack.length - 1] !== lastBracket)	stack.push(bracket+1);
+        else{
+            if(stack.length === 0 || stack.pop() !== lastBracket){
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
