8eef13911e16690e1adeab9e06c08f31c7d99df7
Artyom <artvirsh@mail.ru>
2018-09-12 12:54:43 +0300

    Desicion edited

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-    let config = [].concat.apply([], bracketsConfig),
-        stack = [],
-        bracket,
-        lastBracket,
-        character;
 
-    for(let i = 0; character = str[i]; i++){
-
-        bracket = config.indexOf(character);
-        lastBracket = config.lastIndexOf(character);
-
-        if(bracket === -1) return false;
-
-        if(bracket % 2 == 0 && stack[stack.length - 1] !== lastBracket)	stack.push(bracket+1);
-        else{
-            if(stack.length === 0 || stack.pop() !== lastBracket){
-                return false;
-            }
-        }
+    let config = [].concat.apply([], bracketsConfig);
+    str = str.split('');
+    
+    for(let i = 0; i < str.length; i++) {      
+      if(config.indexOf(str[i]) + 1 === config.lastIndexOf(str[i + 1])) {
+      	str.splice(i, 2);
+        i = -1;
+      }
     }
 
-    return stack.length === 0;
+    return str.length === 0;
 }
